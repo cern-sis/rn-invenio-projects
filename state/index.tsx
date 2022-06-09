2f3ac0ae2090ab8811eb396de95dfde25d37315d
papadopan <antonios.papadopan@gmail.com>
2022-06-09 11:24:33 +0200

    ui: add toggler for the theme
    * closes #14
    
    Signed-off-by: papadopan <antonios.papadopan@gmail.com>

--- state/index.tsx
+++ state/index.tsx
@@ -1,7 +1,10 @@
-import create from 'zustand'
-import { ChangeTitleHookProps } from '../types'
+import create from 'zustand';
+import {ChangeTitleHookProps} from '../types';
 
 export const useStore = create<ChangeTitleHookProps>(set => ({
-    title: "Search",
-    changeTitle: (newTitle: string) => set(state => ({ title: newTitle })),
-}))
+  title: 'Search',
+  changeTitle: (newTitle: string) => set(state => ({title: newTitle})),
+
+  darkTheme: true,
+  setDarkTheme: (theme: boolean) => set(state => ({darkTheme: theme})),
+}));
